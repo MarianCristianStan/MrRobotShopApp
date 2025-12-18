27610fc74610f92ffc582721ba18800dd17e834a
MarianCristianStan <marian.cristian.stan11@gmail.com>
2025-12-18 22:37:00 +0200

    modify program.cs

--- RobotShop/Program.cs
+++ RobotShop/Program.cs
@@ -17,12 +17,9 @@ if (!app.Environment.IsDevelopment())
 {
    app.UseExceptionHandler("/Home/Error");
    app.UseHsts();
+   app.UseHttpsRedirection();
 }
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseHttpsRedirection();
-}
 
 app.UseStaticFiles();
 app.UseRouting();
